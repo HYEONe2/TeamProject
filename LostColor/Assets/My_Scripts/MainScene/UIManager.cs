f35d1eb4651f563b41a444dfc302f87d1149f0f3
HYEONe2 <31698845+HYEONe2@users.noreply.github.com>
2020-04-28 02:20:00 +0900

    1st Commit
    .

---
+++ LostColor/Assets/My_Scripts/MainScene/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject SettingPanel;
+    [SerializeField] private GameObject LogoButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickEnd_SettingButton()
+    {
+        SettingPanel.SetActive(true);
+    }
+
+    public void ClickEnd_CloseButton()
+    {
+        SettingPanel.SetActive(false);
+    } 
+
+    public void ClickEnd_LogoButton()
+    {
+        SceneManager.LoadScene("LogIn");
+        Debug.Log("DLKFJSLEKF");
+        return;
+    }
+}
